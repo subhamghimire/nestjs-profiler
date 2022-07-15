ff3b0878d897b6da609b30f09e1a9f2731b26f58
subhamghimire <subham.ghimire26@gmail.com>
2022-07-15 09:24:14 +0545

    modify validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,15 @@
-import { IsString } from 'class-validator';
-
+import { IsEmail, IsOptional, IsNotEmpty, IsString } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   readonly name: string;
 
-  @IsString()
+  @IsEmail()
   readonly email: string;
 
-  @IsString()
+  @IsNotEmpty()
   readonly password: string;
 
+  @IsOptional()
   @IsString()
   readonly avatar: string;
 
